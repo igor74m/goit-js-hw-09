aab607920969559f7c88f646370dae0b38ead588
igor74m <imixaim74@gmail.com>
2023-03-14 22:58:33 +0200

    fixed js files

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,19 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+const startButton = document.querySelector('[data-start]');
+const stoptButton = document.querySelector('[data-stop]');
+
+let intervalId = null;
+
+startButton.addEventListener('click', (e) => {
+   intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+   }, 1000);
+   startButton.setAttribute('disabled', true);
+});
+   
+stoptButton.addEventListener('click', (e) => {
+  clearInterval(intervalId);
+  startButton.removeAttribute('disabled');
+});
---
+++ src/js/03-promises.js
@@ -0,0 +1,46 @@
+
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+Notify.init({ cssAnimationStyle: 'from-top', fontAwesomeIconStyle: 'shadow' });
+
+const refs = {
+  form: document.querySelector('.form'),
+  // startDelay:document.querySelector('input[name = "delay"]'),
+  // step:document.querySelector('input[name = "step"]'),
+  // amount:document.querySelector('input[name = "amount"]'),
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  const startDelay = Number(refs.form.delay.value);
+  const step = Number(refs.form.step.value);
+  const amount = Number(refs.form.amount.value);
+  
+  for (let i = 1; i <= amount; i += 1){
+    const delayStep =startDelay + step * (i - 1);
+    createPromise(i, delayStep).then(onSuccess).catch(onError)
+  };
+};
+
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+
+  return new Promise((fulfill, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        fulfill({ position, delay })
+      } else {
+        reject({ position, delay })
+      }
+    }, delay);
+  });
+};
+
+function onSuccess({ position, delay }) { 
+  Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+};
+
+function onError({ position, delay }) {
+  Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+};
